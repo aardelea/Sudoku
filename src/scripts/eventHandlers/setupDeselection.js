440e4caf36fc6277f9a8f8d9e64241a9978a9626
aardelea <aardelean2018@gmail.com>
2023-09-03 10:46:17 +0100

    removing comments and correct tabbing

--- src/scripts/eventHandlers/setupDeselection.js
+++ src/scripts/eventHandlers/setupDeselection.js
@@ -3,7 +3,6 @@ import { deselectAllCells } from '/src/scripts/utils.js';
 
 
 export function setupDeselection() {
-    // Clear highlighting when clicking outside of the grid
     document.addEventListener('click', event => {
         if (!sudokuElements.sudokuGrid.contains(event.target)) {
             deselectAllCells();
--- src/scripts/eventHandlers/setupMouseUpEvents.js
+++ src/scripts/eventHandlers/setupMouseUpEvents.js
@@ -2,9 +2,9 @@ import { sudokuElements, mouseState } from '/src/scripts/config.js';
 
 
 export function setupMouseUpEvents() {
-  sudokuElements.cells.forEach(cell => {
-      cell.addEventListener('mouseup', () => {
-          mouseState.isMouseDown = false; 
-      });
-  });
+    sudokuElements.cells.forEach(cell => {
+        cell.addEventListener('mouseup', () => {
+            mouseState.isMouseDown = false; 
+        });
+    });
 };
