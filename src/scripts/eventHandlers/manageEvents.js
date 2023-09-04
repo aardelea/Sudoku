88021b76ecfe2ff439aa90fbcd69e7194d80077c
aardelea <aardelean2018@gmail.com>
2023-09-04 08:42:04 +0100

    refactor: clicks folder

--- src/scripts/eventHandlers/manageEvents.js
+++ src/scripts/eventHandlers/manageEvents.js
@@ -1,10 +1,10 @@
 import { setupMouseDownEvents } from '/src/scripts/eventHandlers/setupMouseDownEvents.js';
 import { setupMouseOverEvents } from '/src/scripts/eventHandlers/setupMouseOverEvents.js';
 import { setupMouseUpEvents } from '/src/scripts/eventHandlers/setupMouseUpEvents.js';
-import { setupClickEvents } from '/src/scripts/eventHandlers/setupClickEvents.js';
-import { setupDoubleClickEvents } from '/src/scripts/eventHandlers/setupDoubleClickEvents.js';
-import { setupEditMode } from '/src/scripts/eventHandlers/setupEditMode.js';
-import { setupDeselection } from '/src/scripts/eventHandlers/setupDeselection.js';
+import { setupClickEvents } from '/src/scripts/eventHandlers/clicks/setupClickEvents.js';
+import { setupDoubleClickEvents } from '/src/scripts/eventHandlers/clicks/setupDoubleClickEvents.js';
+import { setupEditMode } from '/src/scripts/eventHandlers/clicks/setupEditMode.js';
+import { setupDeselection } from '/src/scripts/eventHandlers/clicks/setupDeselection.js';
 import { setupKeydownEvents } from '/src/scripts/eventHandlers/setupKeydownEvents.js';
 import { setupKeyupEvents } from '/src/scripts/eventHandlers/setupKeyupEvents.js';
 import { sudokuElements } from '/src/scripts/config.js';
