6bb32cf56ceae95190e912f105cafac2a3c99a05
aardelea <aardelean2018@gmail.com>
2023-09-08 10:43:53 +0100

    fix bug in highlighting

--- src/scripts/eventHandlers/clickHighlighting.js
+++ src/scripts/eventHandlers/clickHighlighting.js
@@ -4,8 +4,8 @@ import { vars } from '/src/scripts/config.js';
 export function clickHighlighting() {
     vars.cells.forEach(cell => {
         cell.addEventListener('click', () => {
-            const row = cell.dataset.row;
-            const col = cell.dataset.col;
+            const row = parseInt(cell.dataset.row, 10);
+            const col = parseInt(cell.dataset.col, 10);
             const startRow = Math.floor((row - 1) / 3) * 3 + 1;
             const startCol = Math.floor((col - 1) / 3) * 3 + 1;
             const endRow = startRow + 2;
