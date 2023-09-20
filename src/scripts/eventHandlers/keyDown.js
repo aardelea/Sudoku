9967fd95e7412f1b60d0b7414ad1e6566ce6dc9d
aardelea <aardelean2018@gmail.com>
2023-09-20 19:50:24 +0100

    fixing ctrl/shift keydown

--- src/scripts/eventHandlers/keyDown.js
+++ src/scripts/eventHandlers/keyDown.js
@@ -2,6 +2,7 @@ import { updateMostRecentActiveCell } from '/src/scripts/eventHandlers/mouseDown
 import { performUndo } from '/src/scripts/utils/performUndo.js';
 import { updateCell } from '/src/scripts/utils/updateCell.js';
 import { arrowKeys } from '/src/scripts/utils/arrowKeys.js';
+import { vars } from '/src/scripts/config.js';
 
 
 export function keyDown() {
@@ -9,6 +10,7 @@ export function keyDown() {
     updateMostRecentActiveCell();
 
     document.addEventListener('keydown', (event) => {
+        vars.ctrlOrShiftPressed = event.ctrlKey || event.shiftKey;
         
         if (/[1-9]/.test(event.key)) {
             updateCell(event.key, event)
