797409b0144ea6a6f2ee21f65af6d25a8e941501
aardelea <aardelean2018@gmail.com>
2023-09-23 18:06:04 +0100

    create account page

--- src/scripts/components/createAccountButton.js
+++ src/scripts/components/createAccountButton.js
@@ -1,5 +1,5 @@
 export function createAccountButton() {
     document.getElementById('create-account').addEventListener('click', () => {
-        
+        window.location.href = '/src/register.html';
     });
 };
