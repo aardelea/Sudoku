8b804829484a6774a4cbfe1e5d84fddf703a3a8a
aardelea <aardelean2018@gmail.com>
2023-10-29 17:27:55 +0000

    CTRL + Y redo

--- src/scripts/eventHandlers/keyDown.js
+++ src/scripts/eventHandlers/keyDown.js
@@ -1,5 +1,6 @@
 import { updateMostRecentActiveCell } from '/src/scripts/eventHandlers/mouseDown.js';
 import { performUndo } from '/src/scripts/components/undoButton.js';
+import { performRedo } from '/src/scripts/components/redoButton.js';
 import { updateCell } from '/src/scripts/utils/updateCell.js';
 import { arrowKeys } from '/src/scripts/utils/arrowKeys.js';
 import { deleteKey } from '/src/scripts/utils/deleteKey.js';
@@ -21,6 +22,8 @@ export function keyDown() {
             deleteKey()
         } else if (event.ctrlKey && event.key === 'z') {
             performUndo(event=event);
+        } else if (event.ctrlKey && event.key === 'y') {
+            performRedo(event=event);
         };
 
     });
