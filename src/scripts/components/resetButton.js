494623a74d3296fa2134b8dfc307f46578a063a1
aardelea <aardelean2018@gmail.com>
2023-10-29 09:32:19 +0000

    reset now working with pencilmarks

--- src/scripts/components/resetButton.js
+++ src/scripts/components/resetButton.js
@@ -1,10 +1,11 @@
 import { vars } from '/src/scripts/config.js';
+import { removeExistingConflictHighlights } from '/src/scripts/utils/removeExistingConflictHighlights.js';
 
 
 export function resetButton() {
     vars.resetButton.addEventListener('click', () => {
+        removeExistingConflictHighlights();
         vars.cells.forEach(cell => {
-            cell.classList.remove('conflict-highlighted');
             if (!cell.classList.contains('fixed')) {
                 cell.textContent = '';
             };
