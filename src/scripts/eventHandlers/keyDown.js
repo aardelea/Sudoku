27403ebe1646284c52eadb995595077c79824bdc
aardelea <aardelean2018@gmail.com>
2023-10-29 18:48:55 +0000

    SHIFT + digit working now

--- src/scripts/eventHandlers/keyDown.js
+++ src/scripts/eventHandlers/keyDown.js
@@ -14,8 +14,29 @@ export function keyDown() {
     document.addEventListener('keydown', (event) => {
         vars.ctrlOrShiftPressed = event.ctrlKey || event.shiftKey;
 
-        if (event.key === 'Alt') {
+        if (event.altKey) {
             event.preventDefault();
+        }
+
+        if (event.shiftKey) {
+            let digit;
+            switch (event.key) {
+                case "!": digit = "1"; break;
+                case "\"": digit = "2"; break;
+                case "£": case "#": digit = "3"; break;
+                case "$": digit = "4"; break;
+                case "%": digit = "5"; break;
+                case "^": digit = "6"; break;
+                case "&": digit = "7"; break;
+                case "*": digit = "8"; break;
+                case "(": digit = "9"; break;
+                case ")": digit = "0"; break;
+            }
+
+            if (digit) {
+                updateCell(digit, event);
+                return;
+            }
         }
         
         if (/^[0-9]$/.test(event.key)) {
