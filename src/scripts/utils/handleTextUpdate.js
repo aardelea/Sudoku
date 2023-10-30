d225194f36fb83308e47ece6d84be6b89c5897ff
aardelea <aardelean2018@gmail.com>
2023-10-30 16:17:51 +0000

    adding both centre and corner digits to cell

--- src/scripts/utils/handleTextUpdate.js
+++ src/scripts/utils/handleTextUpdate.js
@@ -2,6 +2,12 @@ import { adjustFontSize } from '/src/scripts/utils/adjustFontSize.js';
 
 
 export function handleTextUpdate(cell, digit, textType) {
+    if (!cell.querySelector('.center-text') && !cell.querySelector('.corner-text')) {
+        if (textType === 'center-text' || textType === 'corner-text') {
+            cell.innerHTML = '';
+        }
+    }
+
     const currentDigits = Array.from(cell.querySelectorAll(`.${textType}`)).map(span => span.textContent);
     if (currentDigits.includes(digit)) {
         const index = currentDigits.indexOf(digit);
@@ -11,8 +17,19 @@ export function handleTextUpdate(cell, digit, textType) {
     };
     currentDigits.sort();
 
-    cell.innerHTML = currentDigits.map(d => `<span class="${textType}">${d}</span>`).join('');
+    Array.from(cell.querySelectorAll(`.${textType}`)).forEach(span => span.remove());
+
+    const fragment = document.createDocumentFragment();
+    currentDigits.forEach(d => {
+        const span = document.createElement('span');
+        span.className = textType;
+        span.textContent = d;
+        fragment.appendChild(span);
+    });
+    cell.appendChild(fragment);
+
     cell.classList.add(textType);
+    
     if (textType === 'center-text') {
         adjustFontSize(cell, currentDigits.length);
     } else if (textType === 'corner-text') {
