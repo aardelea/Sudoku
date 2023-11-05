413d4c7ce62e940929decfe7c373d49f96081a44
aardelea <aardelean2018@gmail.com>
2023-11-05 19:42:11 +0000

    grid and keypad js fixing

--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -1,7 +1,5 @@
+import { vars } from '/src/scripts/config.js';
 import { manageGrid } from './components/manageGrid.js';
-
-import { homeButton } from '/src/scripts/components/homeButton.js';
-
 import { mouseDown } from '/src/scripts/eventHandlers/mouseDown.js';
 import { mouseOver } from '/src/scripts/eventHandlers/mouseOver.js';
 import { mouseUp } from '/src/scripts/eventHandlers/mouseUp.js';
@@ -9,28 +7,12 @@ import { clickDouble } from '/src/scripts/eventHandlers/clickDouble.js';
 import { clickEditMode } from '/src/scripts/eventHandlers/clickEditMode.js';
 import { clickDeselectAllCells } from '/src/scripts/eventHandlers/clickDeselectAllCells.js';
 import { keyDown } from '/src/scripts/eventHandlers/keyDown.js';
-import { keyPad } from '/src/scripts/components/keyPad.js';
 import { keyUp } from '/src/scripts/eventHandlers/keyUp.js';
-import { vars } from '/src/scripts/config.js';
-
-import { resetButton } from '/src/scripts/components/resetButton.js';
-import { deleteButton } from '/src/scripts/components/deleteButton.js';
-import { undoButton } from '/src/scripts/components/undoButton.js';
-import { redoButton } from '/src/scripts/components/redoButton.js';
-
-import { manageButtonStates } from '/src/scripts/eventHandlers/manageButtonStates.js';
-import { digitsCentreButton } from '/src/scripts/components/digitsCentreButton.js';
-import { digitsNormalButton } from '/src/scripts/components/digitsNormalButton.js';
-import { digitsCornerButton } from '/src/scripts/components/digitsCornerButton.js';
-import { digitsColourButton } from '/src/scripts/components/digitsColourButton.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    homeButton();
-
-    manageGrid();
-
     vars.cells = vars.sudokuGrid.querySelectorAll('.cell');
+    manageGrid();
     mouseDown();
     mouseOver();
     mouseUp();
@@ -39,16 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
     keyUp();
     clickDeselectAllCells();
     clickEditMode();
-    keyPad();
-    
-    resetButton();
-    deleteButton();
-    undoButton();
-    redoButton();
-
-    manageButtonStates()
-    digitsNormalButton();
-    digitsCentreButton();
-    digitsCornerButton();
-    digitsColourButton();
 });
--- src/scripts/keypad.js
+++ src/scripts/keypad.js
@@ -1,10 +1,8 @@
 import { keyPad } from '/src/scripts/components/keyPad.js';
-
 import { resetButton } from '/src/scripts/components/resetButton.js';
 import { deleteButton } from '/src/scripts/components/deleteButton.js';
 import { undoButton } from '/src/scripts/components/undoButton.js';
 import { redoButton } from '/src/scripts/components/redoButton.js';
-
 import { manageButtonStates } from '/src/scripts/eventHandlers/manageButtonStates.js';
 import { digitsCentreButton } from '/src/scripts/components/digitsCentreButton.js';
 import { digitsNormalButton } from '/src/scripts/components/digitsNormalButton.js';
