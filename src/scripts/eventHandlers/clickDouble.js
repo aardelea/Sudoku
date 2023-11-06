9710eaf6da3e8e7220c292b357d1947469c9abeb
aardelea <aardelean2018@gmail.com>
2023-11-06 18:11:08 +0000

    fixing double click on cell

--- src/scripts/eventHandlers/clickDouble.js
+++ src/scripts/eventHandlers/clickDouble.js
@@ -5,6 +5,7 @@ export function clickDouble() {
     vars.cells.forEach(cell => {
         cell.addEventListener('dblclick', () => {
             if (cell.textContent !== '') {
+                cell.classList.add('clicked');
                 vars.cells.forEach(otherCell => {
                     if (otherCell !== cell && otherCell.textContent === cell.textContent) {
                         otherCell.classList.add('clicked');
