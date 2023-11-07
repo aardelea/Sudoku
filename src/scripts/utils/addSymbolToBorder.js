8aa662f665883d048fddf6561417c11695ed5130
aardelea <aardelean2018@gmail.com>
2023-11-07 22:11:25 +0000

    making X and V work together

--- src/scripts/utils/addSymbolToBorder.js
+++ src/scripts/utils/addSymbolToBorder.js
@@ -1,41 +1,44 @@
+import { vars } from '/src/scripts/config.js';
+
+
 export function addSymbolToBorder(cell, direction, grid) {
     const row = parseInt(cell.dataset.row, 10);
     const col = parseInt(cell.dataset.col, 10);
 
-    let preventX = false;
+    let preventSymbol = false;
     if (direction === 'bottom' && row < 9) {
         const cellBelow = grid.querySelector(`.cell[data-row="${row + 1}"][data-col="${col}"]`);
         if (cellBelow && cellBelow.querySelector('.symbol-top')) {
-            preventX = true;
+            preventSymbol = true;
         };
     } else if (direction === 'top' && row > 1) {
         const cellAbove = grid.querySelector(`.cell[data-row="${row - 1}"][data-col="${col}"]`);
         if (cellAbove && cellAbove.querySelector('.symbol-bottom')) {
-            preventX = true;
+            preventSymbol = true;
         };
     }else if (direction === 'right' && col < 9) {
         const cellRight = grid.querySelector(`.cell[data-row="${row}"][data-col="${col + 1}"]`);
         if (cellRight && cellRight.querySelector('.symbol-left')) {
-            preventX = true;
+            preventSymbol = true;
         };
     } else if (direction === 'left' && col > 1) {
         const cellLeft = grid.querySelector(`.cell[data-row="${row}"][data-col="${col - 1}"]`);
         if (cellLeft && cellLeft.querySelector('.symbol-right')) {
-            preventX = true;
+            preventSymbol = true;
         };
     };
 
-    if (preventX) {
+    if (preventSymbol) {
         return;
     };
 
     if (cell.querySelector(`.symbol-${direction}`)) {
-        const existingX = cell.querySelector(`.symbol-${direction}`);
-        cell.removeChild(existingX);
+        const existingSymbol = cell.querySelector(`.symbol-${direction}`);
+        cell.removeChild(existingSymbol);
     } else {
-        const xMarker = document.createElement('div');
-        xMarker.classList.add('symbol-marker', `symbol-${direction}`);
-        xMarker.textContent = 'X';
-        cell.appendChild(xMarker);
+        const symbolMarker = document.createElement('div');
+        symbolMarker.classList.add('symbol-marker', `symbol-${direction}`);
+        symbolMarker.textContent = vars.puzzleSetterCurrentSymbol;
+        cell.appendChild(symbolMarker);
     };
 };
