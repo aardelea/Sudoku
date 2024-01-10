ebd5ef763c7bd755199a3ec9cfe28f36ce309595
aardelea <aardelean2018@gmail.com>
2024-01-10 15:03:33 +0000

    sign-up verification code

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,34 +1,62 @@
 import { Amplify } from 'aws-amplify';
-import { signUp } from 'aws-amplify/auth';
+import { signUp, confirmSignUp } from 'aws-amplify/auth';
 import awsmobile from 'scripts/aws-exports.js';
 
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('create-account').addEventListener('click', async (event) => {
-        event.preventDefault();
-
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-
-        try {
-            Amplify.configure(awsmobile);
-            
-            const { isSignUpComplete, userId, nextStep } = await signUp({
-                username: email,
-                password,
-                options: {
-                    userAttributes: {
-                        email
-                    },
-                    autoSignIn: true
-                }
-            });
-
-            document.getElementById('registrationMessage').textContent = 'Registration successful! Check your email for verification.';
-        } catch (error) {
-            console.error('Error signing up:', error);
-            document.getElementById('registrationMessage').textContent = error.message;
-        }
-    });
+    document.getElementById('create-account').addEventListener('click', handleSignUp);
+    document.getElementById('verify-account').addEventListener('click', handleVerify);
 });
+
+
+async function handleSignUp(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        Amplify.configure(awsmobile);
+        
+        const { userConfirmed, user  } = await signUp({
+            username: email,
+            password,
+            options: {
+                userAttributes: {
+                    email
+                },
+                autoSignIn: true
+            }
+        });
+
+        if (!userConfirmed) {
+            document.getElementById('verification-section').style.display = 'flex';
+        }
+
+        document.getElementById('registrationMessage').textContent = 'Check your email for a verification code to complete registration.';
+    } catch (error) {
+        console.error('Error signing up:', error);
+        document.getElementById('registrationMessage').textContent = error.message;
+    };
+};
+
+
+async function handleVerify(event) {
+    event.preventDefault();
+    const username = document.getElementById('email').value;
+    const confirmationCode = document.getElementById('verification-code').value;
+
+    try {
+        await confirmSignUp({
+            username,
+            confirmationCode
+        });
+        document.getElementById('registrationMessage').textContent = 'Account verified! Redirecting...';
+        setTimeout(() => {
+            window.location.href = '/home.html';
+        }, 2000); // Redirect after 2 seconds
+    } catch (error) {
+        console.error('Error verifying account:', error);
+        document.getElementById('registrationMessage').textContent = error.message;
+    }
+}
