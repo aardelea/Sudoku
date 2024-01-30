a62ec765cfd6ee37b3b04eaf5a63a06b0ca08e36
aardelea <aardelean2018@gmail.com>
2024-01-30 10:08:57 +0000

    small auth refactor

--- src/scripts/components/logInButton.js
+++ src/scripts/components/logInButton.js
@@ -1,6 +1,27 @@
-import { authLogin } from '../utils/authLogin.js';
+import { signIn } from 'aws-amplify/auth';
 
 
 export function logInButton() {
     document.getElementById('log-in').addEventListener('click', authLogin);
+};
+
+
+async function authLogin(event) {
+    event.preventDefault();
+
+    try {
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        await signIn({
+            username: email,
+            password,
+        });
+        document.getElementById('registrationMessage').textContent = 'Login successful! Redirecting...';
+        setTimeout(() => {
+            window.location.href = '/home.html';
+        }, 2000); // Redirect after 2 seconds
+    } catch (error) {
+        console.error('Error signing in:', error);
+        document.getElementById('registrationMessage').textContent = error.message;
+    };
 };
--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,8 +1,65 @@
-import { authSignUp } from './utils/authSignUp.js';
-import { authConfirmSignUp } from './utils/authConfirmSignUp.js';
+
+import { Amplify } from 'aws-amplify';
+import { signUp } from 'aws-amplify/auth';
+import { confirmSignUp } from 'aws-amplify/auth';
+
+import awsmobile from 'scripts/aws-exports.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('create-account').addEventListener('click', authSignUp);
     document.getElementById('verify-account').addEventListener('click', authConfirmSignUp);
 });
+
+
+async function authSignUp(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        Amplify.configure(awsmobile);
+        
+        const { userConfirmed  } = await signUp({
+            username: email,
+            password,
+            options: {
+                userAttributes: {
+                    email
+                },
+                autoSignIn: true
+            }
+        });
+
+        if (!userConfirmed) {
+            document.getElementById('verification-section').style.display = 'flex';
+        }
+
+        document.getElementById('registrationMessage').textContent = 'Check your email for a verification code to complete registration.';
+    } catch (error) {
+        console.error('Error signing up:', error);
+        document.getElementById('registrationMessage').textContent = error.message;
+    };
+};
+
+
+async function authConfirmSignUp(event) {
+    event.preventDefault();
+    const username = document.getElementById('email').value;
+    const confirmationCode = document.getElementById('verification-code').value;
+
+    try {
+        await confirmSignUp({
+            username,
+            confirmationCode
+        });
+        document.getElementById('registrationMessage').textContent = 'Account verified! Redirecting...';
+        setTimeout(() => {
+            window.location.href = '/home.html';
+        }, 2000); // Redirect after 2 seconds
+    } catch (error) {
+        console.error('Error verifying account:', error);
+        document.getElementById('registrationMessage').textContent = error.message;
+    };
+};
