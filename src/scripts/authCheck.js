096525649eb6ce66f5b84f241cca9479719c60b7
aardelea <aardelean2018@gmail.com>
2024-01-30 23:03:37 +0000

    if you are not logged in, two pages are now not available

--- src/scripts/authCheck.js
+++ src/scripts/authCheck.js
@@ -11,20 +11,39 @@ document.addEventListener('DOMContentLoaded', async () => {
     try {
         Amplify.configure(awsmobile);
         const { idToken } = (await fetchAuthSession()).tokens ?? {};
-        const authMessage = `<div class="auth-message-container">Welcome back, ${idToken.payload.email}!</div> <button class="auth-button" id="sign-out">Sign out</button><p id="registrationMessage"></p>`;
-        renderAuthContainer(authMessage);
+
+        if (!['/my-puzzles.html', '/setter-mode.html'].includes(window.location.pathname)) {
+            const authMessage = `<div class="auth-message-container">Welcome back, ${idToken.payload.email}!</div> <button class="auth-button" id="sign-out">Sign out</button><p id="registrationMessage"></p>`;
+            renderAuthContainer(authMessage);
+        } else {
+            document.getElementById('content-container').style.display = 'flex';
+        }
 
     } catch (err) {
-        const notLoggedInMessage = `
-            <button class="auth-button" id="log-in">Log In</button>
-            <div class="auth-input-container">
-                <input type="text" id="email" placeholder="Email">
-                <input type="password" id="password" placeholder="Password">
-                <p id="registrationMessage"></p>
-            </div>
-            <button class="auth-button" id="create-account-page">Create account</button>
-        `;
+        console.log("Not logged in");
+        let notLoggedInMessage = "";
+
+        if (!['/my-puzzles.html', '/setter-mode.html'].includes(window.location.pathname)) {            
+            notLoggedInMessage = `
+                <button class="auth-button" id="log-in">Log In</button>
+                <div class="auth-input-container">
+                    <input type="text" id="email" placeholder="Email">
+                    <input type="password" id="password" placeholder="Password">
+                    <p id="registrationMessage"></p>
+                </div>
+                <button class="auth-button" id="create-account-page">Create account</button>
+            `;
+            
+        } else {
+            document.getElementById('content-container').style.display = 'none';
+            notLoggedInMessage = `
+                <div class="auth-message-container" style="margin-top: 20%;">
+                    You must be logged in to access this section.
+                </div>
+            `;
+        };
         renderAuthContainer(notLoggedInMessage);
+        
     };
     
 });
